fcc918ef16f2f40cecd8fce2066e00feede8dd5d
yuri guemra <yuri.guemra@live.fr>
2024-02-06 09:40:14 +0100

    chapitre 9

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -5,6 +5,7 @@ import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 
+
 <div className={styles.shape} />;
 
 export default function Page() {
